8571439ceadb1a0baa5739b629336aedccc8a84d
Thanh Nghi <thanhnghi1421@gmail.com>
2022-12-24 22:19:34 +0700

    fix deploy

--- main.py
+++ main.py
@@ -21,19 +21,19 @@ labels = ['pos', 'nev']
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     # answer = request.args.get("answer_mood")
-    token_label1, sam_label1 = load_data(url_full_train_data, "Sheet1", 300)
-    label1_pred = predict(request.form.get('answer_mood'), token_label1, sam_label1, load_aspect_model('CNN/CNN_train_3c_relu.json',
-                                                                'CNN/dts-phuclong_raw_train_2c-001-0.0144-1.0000.h5'),
-                          labels)
-    data = importdata()
-    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
-    clf_entropy = train_using_entropy(X_train, X_test, y_train)
-
-    if label1_pred == 'nev':
-        mood = 1
-    else:
-        mood = 0
-    y_pred_entropy = clf_entropy.predict([[1,0.8,0.8,1]])
+    # token_label1, sam_label1 = load_data(url_full_train_data, "Sheet1", 300)
+    # label1_pred = predict(request.form.get('answer_mood'), token_label1, sam_label1, load_aspect_model('CNN/CNN_train_3c_relu.json',
+    #                                                             'CNN/dts-phuclong_raw_train_2c-001-0.0144-1.0000.h5'),
+    #                       labels)
+    # data = importdata()
+    # X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+    # clf_entropy = train_using_entropy(X_train, X_test, y_train)
+    #
+    # if label1_pred == 'nev':
+    #     mood = 1
+    # else:
+    #     mood = 0
+    # y_pred_entropy = clf_entropy.predict([[1,0.8,0.8,1]])
     # return json.dumps({'name': y_pred_entropy[0]})
 
     if request.method == 'POST':
